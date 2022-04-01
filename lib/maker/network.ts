990e59702b9fd8021e779ef0706d793e3ce9643f
velerofinance <imdipperpines3199@gmail.com>
2022-04-01 15:30:13 +0300

    fix

--- lib/maker/network.ts
+++ lib/maker/network.ts
@@ -3,12 +3,12 @@ import { SupportedNetworks } from '../constants';
 export function networkToRpc(network: SupportedNetworks, nodeProvider?: 'infura' | 'alchemy'): string {
   switch (network) {
     case SupportedNetworks.VELAS:
-      return 'https://explorer.velas.com/rpc';
+      return 'https://evmexplorer.velas.com/rpc';
     case SupportedNetworks.VELASTESTNET:
       return 'https://explorer.testnet.velas.com/rpc';
     case SupportedNetworks.TESTNET:
       return 'https://explorer.testnet.velas.com/rpc';
     default:
-      return 'https://explorer.velas.com/rpc';
+      return 'https://evmexplorer.velas.com/rpc';
   }
 }
